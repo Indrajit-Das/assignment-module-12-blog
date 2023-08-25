6ad2ef40c16c6302aa4401ab3bd48f4630b26b7d
Indrajit-Das <indrojit210210@gmail.com>
2023-08-26 01:47:41 +0600

    some change in timestamps

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -4,13 +4,20 @@ import Image from "next/image";
  const SingleBlogPost = async ({params}) => {
     const id = params.id;
     const data= await getSinglePost(id);
+    const formatDate=(timestamps)=>{
+        let date = new Date(timestamps);
+        const fullYear = date.getFullYear();
+        const month = String(date.getMonth()+1).padStart(2,'0');
+        const day = String(date.getDay()+1).padStart(2,'0');
+        return `${day}-${month}-${fullYear}`
+    }
     return (
         <div className="p-10">
             {data.postDetails !== null && (
                 <div>
                     <div className="flex justify-between">   
                         <div></div>
-                        <span className="ml-auto">Created-At :&nbsp; {data?.postDetails?.created_at}</span>
+                        <span className="ml-auto">Created-At :&nbsp; {formatDate(data?.postDetails?.created_at)}</span>
                     </div>
                     <Image src={data?.postDetails?.img} width="300" height="600" alt={data?.postDetails?.title} />
                     <h2 className='font-xl font-bold mb-5 underline underline-offset-8 mt-5'>{data?.postDetails?.title}</h2>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,12 +4,23 @@ import Image from "next/image";
 
 const Blog = async()=> {
     const data = await getPosts();
+    const formatDate=(timestamps)=>{
+        let date = new Date(timestamps);
+        const fullYear = date.getFullYear();
+        const month = String(date.getMonth()+1).padStart(2,'0');
+        const day = String(date.getDay()+1).padStart(2,'0');
+        return `${day}-${month}-${fullYear}`
+    }
     return (
         <div>
             {data.map((element)=>{
                 return (
                     <div key={element.id} className='p-2 m-2 border-2 border-slate-800 rounded-lg'>
                         <Link href={`blog/${element.id}`}>
+                            <div className="flex justify-between">
+                                <div></div>
+                                <span>{formatDate(element.created_at)}</span>
+                            </div>
                             <div className="flex">
                                 <Image src={element.img} height="200" width="300" alt={element.title} />
                                 <p className='p-1 ml-5'>Title -&gt;&nbsp;{element.title}</p>
